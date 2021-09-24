9ed249e8b4bcba7e6875a1a2f2eaed7bba188e27
bkfwebdev <bryant.feld@gmail.com>
2021-09-24 16:36:28 -0400

    added separate input processing function, simplified cat and webcat

--- step3.js
+++ step3.js
@@ -4,41 +4,50 @@ let myInput = process.argv;
 let arg1 = myInput[2];
 let arg2 = myInput[3];
 let arg2 = myInput[4];
-let isURL = /http/i
+let isURL = /http/i;
+let isWriteFlag = /--out/i;
+let isFileName = /^[-\w^&'@{}[\],$=!#().%+~ ]+$/;
 
+const processInput = (firstArg, secondArg, thirdArg) => {
+  if (isURL.test(firstArg)) {
+    /* write contents of url to console */
+  }
 
-const cat = () => {
-fileSystem.readFile(arg1,"utf8",(err,data)=>{
-    if (err){
-        console.log(err);
-        process.exit(1);
+  if (isWriteFlag.test(firstArg) && isURL.test(secondArg)) {
+    /* write url contents to a file name given in thirdArg */
+  }
+
+  if (isFileName.test(firstArg)) {
+    /* write data to console */
+  }
+
+  if (isWriteFlag.test(firstArg) && isFileName.test(secondArg)) {
+    /* write data to file file name given in thirdArg */
+  }
+};
+
+const cat = (arg) => {
+  fileSystem.readFile(arg, "utf8", (err, data) => {
+    if (err) {
+      console.log(err);
+      process.exit(1);
     } else {
-        if(arg2 == "--out"){fileSystem.writeFile(arg3),data,(err)=>{if(err){
-            console.log(err);
-            process.exit(1)}
-        }
-    }else {
-        console.log(data);
+      return data;
     }
-}
-}
-}
+  });
+};
 
 const webCat = () => {
-    axios.get(arg1)
-  .then(function (response) {
-    // handle success
-    
-    console.log(response);
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-    process.exit(1)
-  });
+  axios
+    .get(arg)
+    .then(function (response) {
+      // handle success
 
-}
-
-
-
-if(isURL){webCat()}else{cat}
+      return response;
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+      process.exit(1);
+    });
+};
