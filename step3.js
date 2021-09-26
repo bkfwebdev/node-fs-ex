d06c395a68e60756e294cd2a94fb97aa283db74c
bkfwebdev <bryant.feld@gmail.com>
2021-09-25 20:38:45 -0400

    functions complete - needs testing

--- step3.js
+++ step3.js
@@ -1,5 +1,6 @@
 let fileSystem = require("fs");
 let axios = require("axios");
+const { findSourceMap } = require("module");
 let myInput = process.argv;
 let arg1 = myInput[2];
 let arg2 = myInput[3];
@@ -11,19 +12,38 @@ let isFileName = /^[-\w^&'@{}[\],$=!#().%+~ ]+$/;
 const processInput = (firstArg, secondArg, thirdArg) => {
   if (isURL.test(firstArg)) {
     /* write contents of url to console */
-  }
-
-  if (isWriteFlag.test(firstArg) && isURL.test(secondArg)) {
-    /* write url contents to a file name given in thirdArg */
-  }
+    let data = webCat(firstArg);
+    console.log(data); 
+  } 
 
   if (isFileName.test(firstArg)) {
     /* write data to console */
-  }
+    let data = cat(firstArg);
+    console.log(data);
+  } 
 
+  if (isWriteFlag.test(firstArg) && isURL.test(secondArg)) {
+    /* write url contents to a file name given in thirdArg */
+    let data = webcat(secondArg);
+    fileSystem.writeFile(thirdArg,data,"utf8",(err)=>{
+        if(err){
+            console.error(err);
+            process.exit(1);
+        }
+    });
+  }
+  
   if (isWriteFlag.test(firstArg) && isFileName.test(secondArg)) {
     /* write data to file file name given in thirdArg */
-  }
+    let data = cat(secondArg);
+    fileSystem.writeFile(thirdArg,data,"utf8",(err)=>{
+        if(err){
+            console.error(err);
+            process.exit(1);
+        }
+    });
+  } 
+
 };
 
 const cat = (arg) => {
